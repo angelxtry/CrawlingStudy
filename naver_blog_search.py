cdbc678b28faaee59e05fe1a35818ae0abcb4650
angelxtry <angelxtry@gmail.com>
2017-06-20 09:46:20 +0900

    naver blog 검색 crawling 수정 중

---
+++ naver_blog_search.py
@@ -0,0 +1,25 @@
+
+from collections import OrderedDict
+from itertools import count
+import requests
+from bs4 import BeautifulSoup
+
+
+url = 'https://search.naver.com/search.naver'
+params = {
+    'where': 'post',
+    'query': 'AskDjango',
+    'start': 1,
+}
+
+response = requests.get(url, params=params)
+html = response.text
+soup = BeautifulSoup(html, 'html.parser')
+tag_list = soup.select('.sh_blog_title')
+
+print(response.request.url)
+print(html.encode('cp949', 'ignore'))
+
+for tag in tag_list:
+    print(tag)
+    print(tag['href'], tag.text)
